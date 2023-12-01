ec1602c2b8c61668e57094fb73aed08e8d173741
mfenton <martin_fenton@hotmail.co.uk>
2023-12-01 16:16:25 +0000

    have login and also collected data

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const DOMAIN = 'https://learn.01founders.co/';
+const DOMAIN = 'https://learn.01founders.co/api/graphql-engine/v1/graphql/';
+// Gitea Access Token = 9fd40d4a6a1776854d2171f943ab2f254b8da113
+
 
 document.addEventListener('DOMContentLoaded', function () {
     const loginForm = document.getElementById('login-form');
@@ -39,6 +41,7 @@ async function handleLogin() {
 
             // Save the JWT to localStorage for future use
             localStorage.setItem("jwt", jwtData);
+
             await useJWT()
         } else {
             // Display an error message or handle unsuccessful login
@@ -53,16 +56,57 @@ async function handleLogin() {
 async function useJWT(){
 
     const jwtToken = localStorage.getItem("jwt");
-    console.log("this is  useJWT - JWTToken:", jwtToken)
 
 // Include the JWT in the headers of your fetch request
-const response = await fetch(`${DOMAIN}api/graphql-engine/v1/graphql`, {
-    method: "GET",
+// const response = await fetch(`${DOMAIN}api/graphql-engine/v1/graphql`, {
+//     method: "GET",
+//     headers: {
+//         Authorization: `Bearer ${jwtToken}`,
+//     },
+// });
+const response = await fetch(DOMAIN, {
+    method: "POST",
     headers: {
         Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
     },
-
-
+    body: JSON.stringify({
+        query: `
+            query {
+                user {
+                    id
+                    login
+                }
+                transaction {
+                    id
+                    type
+                    amount
+                    userId
+                }
+                progress {
+                    id
+                    userId
+                    objectId
+                    grade
+                }
+                result {
+                    id
+                    objectId
+                    userId
+                    grade
+                }
+                object {
+                    id
+                    name
+                    type
+                    attrs
+                }
+            }
+        `
+    }),
 });
-console.log("response:", response)
+    
+
+console.log("Response Content:", await response.json());
+
 }
